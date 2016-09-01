87798ded8845ce2c7289383d79a17982539ee073
Phanidhar K Bhamidi <bkphanidhar@gmail.com>
2016-09-01 16:38:56 +0530

    order microservice changes
    order microservice changes

--- ordersms/src/main/java/com/sg/training/spring/cloud/OrderMicroServiceApplication.java
+++ ordersms/src/main/java/com/sg/training/spring/cloud/OrderMicroServiceApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages="com.sg.training.spring.cloud.repository")
 @EnableDiscoveryClient
+@EnableResourceServer
 public class OrderMicroServiceApplication {
 
 	public static void main(String[] args) {
